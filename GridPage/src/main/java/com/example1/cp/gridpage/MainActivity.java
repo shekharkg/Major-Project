0b8484401867935fab7ae565f02296086ce0dcb8
Shekhar Kumar Gupta <shekhar114@gmail.com>
2014-03-26 01:07:12 +0530

    JSON data passed to GridView (lots of errors)

--- GridPage/src/main/java/com/example1/cp/gridpage/MainActivity.java
+++ GridPage/src/main/java/com/example1/cp/gridpage/MainActivity.java
@@ -1,33 +1,15 @@
 package com.example1.cp.gridpage;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.etsy.android.grid.StaggeredGridView;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
--- GridPage/src/main/java/com/example1/cp/gridpage/SampleData.java
+++ GridPage/src/main/java/com/example1/cp/gridpage/SampleData.java
@@ -1,22 +1,107 @@
 package com.example1.cp.gridpage;
 
-        import java.util.ArrayList;
+import android.os.AsyncTask;
+import android.util.Log;
 
-public class SampleData  {
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    public static final int SAMPLE_DATA_ITEM_COUNT = 30;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+public class SampleData {
+    //static String url = "http://0.us-east-1a.search-sandbox.ss1.mobile.brmob.net:7090/solr/searsoutlet_com_products/select_qd20130731?q=fridge&facet=false&wt=json&fl=product_id%2Cproduct_name%2Cthumb_image_url%2Cmfr_name%2Cprice&";
+    public static final int SAMPLE_DATA_ITEM_COUNT = 5;
+    JSONArray productList = null;
+    static String title = "product title";
+    static String description = "product description";
+    static String image = "http://image.shutterstock.com/display_pic_with_logo/849265/111971633/stock-…to-concept-error-on-white-background-page-not-found-d-render-111971633.jpg";
+    static ArrayList<ProductData> data = new ArrayList<ProductData>(SAMPLE_DATA_ITEM_COUNT);
     public static ArrayList<ProductData> generateSampleData() {
-        final ArrayList<ProductData> data = new ArrayList<ProductData>(SAMPLE_DATA_ITEM_COUNT);
-        String title = "product title";
-        String description = "product description";
-        String image = "http://images.kooves.com/uploads/products/47765_390bb862e68488c956d5c4c75fc8b4ca_image1_thumb_90_120.jpg";
 
+
+        new HttpAsyncTask().execute("http://0.us-east-1a.search-sandbox.ss1.mobile.brmob.net:7090/solr/searsoutlet_com_products/select_qd20130731?q=fridge&facet=false&wt=json&fl=product_id%2Cproduct_name%2Cthumb_image_url%2Cmfr_name%2Cprice&start=0&rows=100");
         for (int i = 0; i < SAMPLE_DATA_ITEM_COUNT; i++) {
-            data.add(new ProductData(title, description,image));
+            data.add(new ProductData(title, description, image));
+        }
+        return data;
+    }
+
+    private static class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return GET(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject json = new JSONObject(result);
+                JSONObject response = json.getJSONObject("response");
+                JSONArray docs = response.getJSONArray("docs");
+
+                for (int i = 0; i < docs.length(); i++) {
+                    title = docs.getJSONObject(i).getString("product_name");
+                    image = docs.getJSONObject(i).getString("thumb_image_url");
+                    description = docs.getJSONObject(i).getString("product_name");
+                    data.add(new ProductData(title, description, image));
+                }
+                //etResponse.setText(str);
+                //etResponse.setText(json.toString(1));
+
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
         }
 
-        return data;
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
     }
 
 }
